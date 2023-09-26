26658fddea40e65564710ac2ff7cf3053f460973
Hugo_Malard <malard.hugo@gmail.com>
2023-09-26 15:49:17 +0200

    readme

--- inference_with_decider.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python BMOHA/inference_decider_whisper.py BMOHA/hparams/cnn/inference_whisper_decider.yaml
--- train_decider.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python BMOHA/train_cnn_latent_space.py BMOHA/hparams/cnn/train_cnn_ponderate.yaml
